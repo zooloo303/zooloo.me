d523f6278a06ee016af7ae671f22f8acba4137f9
zooloo303 <158102111+zooloo303@users.noreply.github.com>
2024-02-29 08:47:17 +0200

    think I've found a d2 lib I can use!

---
+++ zooloo/my_guardian/templates/find_user.py
@@ -0,0 +1,40 @@
+import pydest
+import asyncio
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+    
+api_key=os.getenv("bungie_token"),
+
+platforms = {'XBOX': 1, 'PLAYSTATION': 2, 'PC': 3}
+
+async def main():
+    """You will need to add your api key!"""
+    destiny = pydest.Pydest(api_key[0])
+
+    platform = None
+    while not platform:
+        user_input = input('Enter your platform (xbox, playstation, or pc): ')
+        if user_input.upper() in platforms.keys():
+            platform = platforms.get(user_input.upper())
+        else:
+            print('Invalid platform.')
+
+    username = input('Enter the username to locate: ')
+    res = await destiny.api.search_destiny_player(platform, username)
+
+    if res['ErrorCode'] == 1 and len(res['Response']) > 0:
+        print("---")
+        print("Player found!")
+        print("Display Name: {}".format(res['Response'][0]['displayName']))
+        print("Membership ID: {}".format(res['Response'][0]['membershipId']))
+    else:
+        print("Could not locate player.")
+
+    await destiny.close()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
